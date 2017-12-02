73fce6578b6e1301c0b556744b30541428519ae5
Patsov, Martin <martinely95@gmail.com>
2017-12-02 21:07:06 +0200

    Added balance

--- src/eu/dreamix/Main.java
+++
@@ -1,9 +0,0 @@
-package eu.dreamix;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hi");
-    }
-}
--- src/londonmet/cs/rmi/credit/Person.java
+++ src/londonmet/cs/rmi/credit/Person.java
@@ -1,40 +1,56 @@
 package londonmet.cs.rmi.credit;
 
-public class Person implements java.io.Serializable{
+public class Person implements java.io.Serializable {
 
-   /**
-	 * 
-	 */
-   private static final long serialVersionUID = 1L;
-   private String firstName;
-   private String secondName;
-   private boolean creditRating;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private String firstName;
+    private String secondName;
+    private boolean creditRating;
 
-   public Person(String firstName, String secondName) {
-	      this.firstName = firstName;
-	      this.secondName = secondName;
-	      this.creditRating = true;
-	  }     
-   
-   public Person(String firstName, String secondName, boolean creditRating) {
-      this.firstName = firstName;
-      this.secondName = secondName;
-      this.creditRating = creditRating;
-  }     
+    private Double balance;
 
-  public String getFirstName() {
-     return firstName;
-  }
+    public Person(String firstName, String secondName, boolean creditRating, Double balance) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.creditRating = creditRating;
+        this.balance = balance;
+}
+    public Person(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.creditRating = true;
+    }
 
-  public String getLastName() {
-     return secondName;
-  }
+    public Person(String firstName, String secondName, boolean creditRating) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.creditRating = creditRating;
+    }
 
-  public boolean getCreditRating() {
-	     return creditRating;
-	  }
-  
-  public String toString() {
-     return (secondName + ", " + firstName);
-  }
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return secondName;
+    }
+
+    public boolean getCreditRating() {
+        return creditRating;
+    }
+
+    public String toString() {
+        return (secondName + ", " + firstName);
+    }
 }
